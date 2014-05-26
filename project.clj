e953123d9687ae84980ff71b2ccedd73fc30bdd4
Henrique Ribeiro Rezende <henriquerrezende@gmail.com>
2014-05-26 18:56:08 +0100

    Adding Schema validations

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.1.8"]
                  [compojure "1.1.6"]
-                 [de.ubercode.clostache/clostache "1.3.1"]]
+                 [de.ubercode.clostache/clostache "1.3.1"]
+                 [prismatic/schema "0.2.2"]]
   :plugins [[lein-ring "0.8.10"]
             [lein-midje "3.1.1"]]
   :profiles
--- src/wondercode/models/project.clj
+++ src/wondercode/models/project.clj
@@ -1,4 +1,10 @@
-(ns wondercode.models.project)
+(ns wondercode.models.project
+  (:require [schema.core :as s]))
+
+(def project
+  {:name s/Str
+   :url s/Str
+   (s/optional-key :tags) [s/Str]})
 
 (def empty-template
   (zipmap [:name
